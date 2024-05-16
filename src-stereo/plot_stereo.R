763ead950ad2ca1becf39ac90dbda1a713f80e4d
Lydia Barnes <lydiabarnes@tuta.io>
2024-05-16 10:03:13 +1000

    adding stereo plots

--- src-stereo/plot_stereo.R
+++ src-stereo/plot_stereo.R
@@ -23,36 +23,61 @@ if (!dir.exists(data_path)) {
     stop(paste0(data_path, " does not exist"))
 }
 
-fnl <- file.path(project_path, "res", paste(paste(version, exp, mes, "transitions", sep = "_"), ".csv",
+fnl <- file.path(project_path, "res", paste(paste(version, exp, mes, "stereotypy", sep = "_"), ".csv",
     sep = ""))
-res <- read_csv(fnl)
+stereo <- read_csv(fnl)
 
-### bi-directional transitions
-p <- res %>%
+### transitions
+p <- stereo %>%
     filter(ses == 2, context == ctx) %>%
     ggplot() + geom_point(aes(x = transition_counts, y = accuracy, colour = factor(subses)), alpha = 0.8, size = 15) + 
     geom_text(aes(x = transition_counts,
     y = accuracy, label = sub), alpha = 0.8, size = 8, position = position_jitter(width = 0.1, height = 0.05)) +
     theme_minimal(base_size = label_sz, base_family = "Roboto") + ylim(0, 1.1) + xlim(0.5, 4.5) + labs(title = "Transitions by accuracy during training",
     x = "Transitions", y = "Accuracy", colour = "Half of session") + scale_colour_brewer(palette = "Greens",
-    labels = unique(res$subses)) + theme(panel.background = element_rect(fill = "white", colour = "white"),
+    labels = unique(stereo$subses)) + theme(panel.background = element_rect(fill = "white", colour = "white"),
     plot.background = element_rect(fill = "white", colour = "white"))
 # p <- ggMarginal(p)
 
-fnl <- file.path(project_path, "fig", paste(paste(version, exp, mes, "transitions-bidir", paste("context",
+fnl <- file.path(project_path, "fig", paste(paste(version, exp, mes, "transitions", paste("context",
     ctx, sep = "-"), sep = "_"), ".png", sep = ""))
 ggsave(fnl, plot = p)
 
-### one-way transitions
-p <- res %>%
+### re-clicks
+p <- stereo %>%
     filter(ses == 2, context == ctx) %>%
-    ggplot() + geom_point(aes(x = transition_tril, y = accuracy, colour = factor(subses)), alpha = 0.8, size = 15) + 
-    geom_text(aes(x = transition_tril,
+    ggplot() + geom_point(aes(x = reclicks, y = accuracy, colour = factor(subses)), alpha = 0.8, size = 15) + 
+    geom_text(aes(x = reclicks,
     y = accuracy, label = sub), alpha = 0.8, size = 8, position = position_jitter(width = 0.1, height = 0.05)) +
-    theme_minimal(base_size = label_sz, base_family = "Roboto") + ylim(0, 1.1) + xlim(0.5, 4.5) + labs(title = "One-way transitions by accuracy during training",
-    x = "Transitions", y = "Accuracy", colour = "Half of session") + scale_colour_brewer(palette = "Greens",
-    labels = unique(res$subses)) + theme(panel.background = element_rect(fill = "white", colour = "white"),
+    theme_minimal(base_size = label_sz, base_family = "Roboto") + ylim(0, 1.1) + xlim(0, 8.5) + labs(title = "Switch trial re-clicks by stay trial accuracy",
+    x = "Re-clicks", y = "Accuracy", colour = "Half of session") + scale_colour_brewer(palette = "Greens",
+    labels = unique(stereo$subses)) + theme(panel.background = element_rect(fill = "white", colour = "white"),
     plot.background = element_rect(fill = "white", colour = "white"))
-fnl <- file.path(project_path, "fig", paste(paste(version, exp, mes, "transitions-unidir", paste("context",
+
+fnl <- file.path(project_path, "fig", paste(paste(version, exp, mes, "reclicks", paste("context",
     ctx, sep = "-"), sep = "_"), ".png", sep = ""))
+ggsave(fnl, plot = p)
+
+### paths
+
+# add test phase accuracy to stereo data frame
+fnl <- file.path(project_path, "res", paste(paste(version, exp, mes, "avg", sep = "_"), ".csv",
+      sep = ""))
+avg <- read_csv(fnl)
+stereo <- stereo %>% group_by(sub,ses,context) %>% summarise(accuracy = mean(accuracy),match_hp = mean(match_hp),match_tsp=mean(match_tsp),overshoot_tsp=mean(overshoot_tsp),overshoot_hp=mean(overshoot_hp)) # collapse stereo across subsessions
+avg <- avg %>% filter(ses==3,switch==0)
+stereo$accuracy_test <- avg$accuracy # add test accuracy to stereo results
+
+p <- stereo %>%
+  filter(ses == 2, context == ctx) %>%
+  ggplot() + geom_point(aes(x = overshoot_hp, y = accuracy_test), alpha = 0.8, size = 15) + 
+  geom_text(aes(x = overshoot_hp,
+  y = accuracy_test, label = sub), alpha = 0.8, size = 8, position = position_jitter(width = 0.1, height = 0.05)) +
+  theme_minimal(base_size = label_sz, base_family = "Roboto") + ylim(0, 1.1) + xlim(-0.1,2.1) + labs(title = "Path overshoot (train) by accuracy (test)",
+  x = "Path overshoot", y = "Accuracy") + 
+  theme(panel.background = element_rect(fill = "white", colour = "white"),
+  plot.background = element_rect(fill = "white", colour = "white"))
+
+fnl <- file.path(project_path, "fig", paste(paste(version, exp, mes, "hp", paste("context",
+  ctx, sep = "-"), sep = "_"), ".png", sep = ""))
 ggsave(fnl, plot = p)
