ce5c1014ffcef951b6f2f9d84126d590311ca975
Lydia Barnes <lydiabarnes@tuta.io>
2024-05-23 15:45:24 +1000

    formatting doors event data for maggi algorithm

---
+++ src-learn/format_data_for_maggi.R
@@ -0,0 +1,83 @@
+format_data_for_maggi <- function(nsub=1,nses=1,ncontext=1){
+
+# lydia barnes, may 2024
+# reads event data from doors task
+# re-codes accuracy so that "success" trials (<=4 clicks) that inc. context-irrelevant doors are counted as failures
+# classifies trials by whether they provide evidence for learning 1, 2, 3, or 4 doors
+
+# sources
+library(tidyverse)
+
+# event data
+events <- read.csv('res/study-01_exp_lt_clicks_evt.csv')
+events <- events %>% filter(sub==nsub, ses==nses, context==ncontext)
+
+# find the first trial in which each door is the target (i.e. people's first opportunity to learn)
+doors <- events %>% filter(door_cc==1) %>% pull(door) %>% unique()
+target_idx <- c(which(diff(events$t)==1),length(events$door))
+targets <- events$door[target_idx]
+first_feedback <- rep(0,1,length(doors))
+for (i in 1:length(doors)){
+  first_feedback[i] <- events$t[target_idx[min(which(targets==doors[i]))]]
+}
+
+# exclude trials w <4 clicks on which all doors were context-relevant
+uninformative <- events %>% group_by(t) %>% summarise(clicks = n(),accuracy = mean(door_cc)) %>% 
+  mutate(exclude = case_when((accuracy==1 & clicks<4) ~ t,.default=0)) %>% filter(exclude!=0) %>% pull(exclude)
+events <- events %>% filter(!(t %in% uninformative))
+
+# preallocate arrays
+trials <- unique(events$t)
+ntrials <- length(trials)
+nstrategies <- 4
+learn_doors <- matrix(0,nstrategies,ntrials)
+
+# for remaining trials, 
+for (i in 1:ntrials){
+  trial <- trials[i]
+  tdata <- events %>% filter(t==trial)
+  
+  # if they haven't had a chance to experience a door as a target, treat it as context-irrelevant
+  if((nses==1) &(trial < max(first_feedback))){
+    chance_doors <- doors[which(first_feedback>=trial)]
+    tdata <- tdata %>% mutate(door_cc = case_when(door %in% chance_doors ~ 0, .default=door_cc))
+  }
+  
+  # if the first door clicked is context-irrelevant, count that as evidence against learning
+  if(tdata$door_cc[1]==0){
+    learn_doors[,i] <- 0
+  }else{
+    # count all doors before their first error as evidence for learning those doors
+    try(first_error <- min(which(diff(tdata$door_cc)==-1)), silent = TRUE)
+    if(is.infinite(first_error)){
+      learn_doors[,i] <- 1 # if first_error doesn't exist, they've learned all four doors
+    }else{
+      these_doors <- unique(tdata$door[1:first_error])
+      door_idx <- match(these_doors,doors)
+      learn_doors[door_idx,i] <- 1
+    }
+  }
+}
+
+# find the order in which they first selected the doors
+first_selection <- rep(0,nstrategies)
+for (i in 1:nstrategies){
+  first_selection[i] <- min(which(learn_doors[i,]==1))
+}
+
+# exclude trials on which previously-selected doors aren't among those selected
+for (i in 1:ntrials){
+  for (j in 1:nstrategies){
+    if(trials[i]>first_selection[j] & learn_doors[j,i]==0){
+      learn_doors[,i] <- 0
+    }
+  }
+}  
+# sort by which doors were selected first
+learn_doors <- learn_doors[order(first_selection),]
+
+# format
+strategies <- data.frame(trials,learn_one=learn_doors[1,],learn_two=learn_doors[2,],learn_three=learn_doors[3,],learn_four=learn_doors[4,])
+return(strategies)
+
+}
--- src-learn/run_maggi.R
+++ src-learn/run_maggi.R
@@ -1,18 +1,31 @@
 # lydia barnes, may 2024
+# applies maggi algorithm. algorithm estimates probability of using a given strategy, weighting by recency.
 
 library(zeallot) #unpack/destructure with %<-%
 source(file.path(getwd(), "src-learn", "get_maggi.R"))
+source(file.path(getwd(), "src-learn", "format_data_for_maggi.R"))
 
 set.seed(17)
+simulation <- FALSE
 
-# synthesise data
-ps <- c(.1, .3, .6, .9) #reasonable steps in the probability of successes over runs of trials
-n <- 25 #trials per run
-data <- unlist(lapply(ps, rbinom, n=n, size=1)) # n * length(ps) trials drawn from a binomial distribution
+if (simulation){
+  # synthesise data
+  ps <- c(.1, .3, .6, .9) #reasonable steps in the probability of successes over runs of trials
+  n <- 25 #trials per run
+  data <- unlist(lapply(ps, rbinom, n=n, size=1)) # n * length(ps) trials drawn from a binomial distribution
+}else{
+  # read real data
+  strategies <- format_data_for_maggi(nsub=1,nses=1,ncontext=1)
+  data <- strategies %>% pull(learn_one)
+}
 
 # calculate recency-weighted probability of finding strategy s
 c(alphas,betas,beta_map,beta_variance) %<-% get_maggi(data)
 
-# view final beta distribution
+# view the final beta distribution
+increments <- seq(0,1,by=.01)
+plot(increments,dbeta(increments,alphas[length(data)],betas[length(data)]),type="l",col="darkgreen") 
+
+# view alphas and betas over time
 plot(1:length(data),alphas,type="l",col="darkgreen")
 points(1:length(data),betas,type="l",col="navy")
