9f9e6457e0ecc502734e095b9d2c4db7b7d693e6
Lydia Barnes <lydiabarnes@tuta.io>
2024-05-23 10:30:22 +1000

    implementing maggi algorithm

---
+++ src-learn/get_maggi.R
@@ -0,0 +1,31 @@
+get_maggi <- function(data,alpha=1,beta=1,decay=.9){
+ 
+  # initialise empty arrays
+  alphas <- rep(0,length(data))
+  betas <- alphas
+  s <- alphas
+  f <- alphas 
+  
+  for (i in 1:length(data)){
+    
+    if(i==1){
+      # record success and failure
+      s[i] <- data[i] 
+      f[i] <- 1-data[i] 
+    }else{
+      # record success and failure, modified by success and failure at our last check
+      s[i] <- decay*s[i-1] + data[i]
+      f[i] <- decay*f[i-1] + 1-data[i]
+    }
+    
+    # update the distribution's parameters
+    alphas[i] <- alpha+s[i]
+    betas[i] <- beta+f[i]
+  }
+
+  beta_mean <- alphas / (alphas+betas)
+  beta_variance <- alphas*betas / ((alphas+betas)^2 * (alphas+betas+1))
+  
+  return(list(alphas,betas,beta_mean,beta_variance))
+
+}
