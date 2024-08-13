06af3c238dcd660ebed3fb9fa9e056113ba8db63
kel-github <getkellygarner@gmail.com>
2024-08-13 18:43:02 +1000

    dirichlet example complete

--- src-dirich/dirich-sim-example.R
+++ src-dirich/dirich-sim-example.R
@@ -65,39 +65,55 @@ with_seed(42, {
 ############################################
 ## step 3 - forming our beliefs about the data
 
-## implementation 1: a bayes factor, the strength of which tells
-## us how unlikely the null probabilities are, relative to the observed 
+## inference #1: a bayes factor, the strength of which tells
+## us how likely the null probabilities are, relative to the observed 
 ## probabilities for that participant
-# Now I want to learn 3 things about the data
-# 1) the probability of the alternate hypothesis (that the expected values
+## the probability of the alternate hypothesis (that the expected values
 # are larger than 1/6, relative to the probability
 # that all thetas are likely to be around 1/6
 # note that a larger value is greater evidence for the alternate hypothesis
 null_hyp <- rep(1/n_routines, times=n_routines)
 null_llike <- ddirichlet(null_hyp, alpha=sim_data$posterior_alphas, log=TRUE) 
-# log likelihood of the null
+# log likelihood of the null, save for each participant for future calcs,
+# as I have had an idea for a second level stat that I think is better than
+# the binomial idea we chatted about
 
 exp_theta <- sim_data$posterior_alphas/sum(sim_data$posterior_alphas) 
-# exp value of thetas (given observations)
+# exp value of thetas (given observations) # as above
 
 exp_llike <- ddirichlet(exp_theta, alpha=sim_data$posterior_alphas, log=TRUE)
-lBF <- exp_llike - null_llike
-
-## but the other thing we can do is ask, what is the probability that
-## any given theta will be greater than chance?
+lBF <- exp_llike - null_llike # this is the BF in log p form 
+BF <- exp(lBF) # this is our bayes factor
 
 
+## inference #2: but the other thing we can do is ask, what is the probability that
+## any given theta will be greater than chance? putting here to chat through/might be
+## the sort of thing a reviewer asks for
 
+# first step is to simulate the participant's distribution many, many times, to
+# get an idea of the like spread of thetas, given the observations for that participant,
+# then we calculate the proportion of times that the random draws were less than or
+# == to chance, any prob value < .05/6 would be considered statistically above chance 
+hyp_dat <- with_seed(42, {
+  rdirichlet(n = 1e5, alpha = sim_data$posterior_alphas) |> 
+    data.frame() |> 
+    set_names(paste("α", 1:6, sep="")) |> 
+    pivot_longer(everything(), values_to="p", names_to="alpha") |> 
+    group_by(alpha) |>
+    summarise(prob = mean(p <= 1/n_routines)) 
+})
 
 ## and the last thing we want to compute is the entropy over the expected
-## probability vector, which gives a nice number quantifying how routiney someone
+## thetas, which gives a nice number quantifying how routiney someone
 ## is
+# Entropy is defined as:
+# H(X) = logB(alpha) + (alpha0 - K)digamma(alpha0) - sum_j=1toK(alpha_j - 1)digamma(alpha_j)
+dir_entropy <- function(alphas){
+  # see definition at https://en.wikipedia.org/wiki/Dirichlet_distribution
+  alpha0 <- sum(alphas)
+  K <- length(alphas)
+  log_beta_alpha <- sum(lgamma(alphas)) - lgamma(alpha0)
+  log_beta_alpha + (alpha0 - K)*digamma(alpha0) - sum((alphas-1)*digamma(alphas))
+}
 
-# 2) I also want the log likelihood of the bayes factor
-
-# 2) the expected value of the thetas for that participant
-exp_theta
-
-# 3) the entropy over probabilities for that participant, as our individual diff
-# measure.
-
+with(sim_data, dir_entropy(posterior_alphas))
