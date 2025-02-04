11d394a8c828949821879bf4f3af51359ddc90f6
Lydia Barnes <lydiabarnes@tuta.io>
2025-02-04 17:37:17 +0000

    generate difference score plots

--- src/vis_data.R
+++ src/vis_data.R
@@ -17,10 +17,27 @@ if(exp=="exp_lt"){
   condition_label <- "Transfer Type"
   condition_levels <- c("Identity","Mixed")
   data <- data %>% rename(condition = transfer)
+  data <- left_join(
+    data, data %>% 
+      select(sub,condition,accuracy, setting_errors, rt, k4) %>% 
+      pivot_wider(names_from = condition, values_from = c(accuracy, setting_errors, rt, k4)) %>% 
+      mutate(accuracy_difference = accuracy_partial - accuracy_complete,
+             setting_errors_difference = setting_errors_partial - setting_errors_complete,
+             rt_difference = rt_partial - rt_complete,
+             k4_difference = k4_partial - k4_complete), 
+    join_by(sub))
 }else{
   condition_label <- "Trial Type"
   condition_levels <- c("Stay","Switch")
   data <- data %>% rename(condition = switch)
+  data <- left_join(
+    data, data %>% 
+      select(sub,condition,accuracy, setting_errors, rt) %>% 
+      pivot_wider(names_from = condition, values_from = c(accuracy, setting_errors, rt)) %>% 
+      mutate(accuracy_difference = accuracy_switch - accuracy_stay,
+             setting_errors_difference = setting_errors_switch - setting_errors_stay,
+             rt_difference = rt_switch - rt_stay), 
+    join_by(sub))
 }
 log_data <- data %>% 
   mutate(
@@ -48,6 +65,19 @@ data %>%
 fnl <- file.path(project_path, "fig", paste(exp,"train-type-accuracy.pdf",sep="_"))
 ggsave(fnl, plot = last_plot(), unit = "cm", width = width, height = width*.5, limitsize = FALSE)
 
+#guide = "none"
+data %>% 
+  ggplot(aes(x = train_type, y = accuracy_difference, fill = train_type)) +
+  geom_violin(alpha=.5, width = .6) +
+  geom_boxplot(position = position_dodge(width = .6), width = .05, linewidth = .7, outlier.alpha = 1,outlier.shape = 21,outlier.size = 2.5,outlier.stroke = NA) +
+  scale_fill_manual(values = wes_palette("IsleofDogs1"), labels = c("Stable Context", "Variable Context")) +
+  scale_x_discrete(labels = c("Stable Context", "Variable Context")) +
+  labs(x = "Training Group", y = "Accuracy Difference", fill = "Training Group") +
+  theme_minimal() +
+  theme_doors()
+fnl <- file.path(project_path, "fig", paste(exp,"train-type-accuracy-difference.pdf",sep="_"))
+ggsave(fnl, plot = last_plot(), unit = "cm", width = width, height = width*.5, limitsize = FALSE)
+
 ### setting errors
 data %>% 
   ggplot(aes(x = condition, y = setting_errors, fill = train_type)) +
@@ -59,6 +89,18 @@ data %>%
   theme_minimal() +
   theme_doors()
 fnl <- file.path(project_path, "fig", paste(exp,"train-type-setting-errors.pdf",sep="_"))
+ggsave(fnl, plot = last_plot(), unit = "cm", width = width, height = width*.5, limitsize = FALSE)
+
+data %>% 
+  ggplot(aes(x = train_type, y = setting_errors_difference, fill = train_type)) +
+  geom_violin(alpha=.5, width = .6) +
+  geom_boxplot(position = position_dodge(width = .6), width = .05, linewidth = .7, outlier.alpha = 1,outlier.shape = 21,outlier.size = 2.5,outlier.stroke = NA) +
+  scale_fill_manual(values = wes_palette("IsleofDogs1"), labels = c("Stable Context", "Variable Context")) +
+  scale_x_discrete(labels = c("Stable Context", "Variable Context")) +
+  labs(x = "Training Group", y = "Setting Errors Difference", fill = "Training Group") +
+  theme_minimal() +
+  theme_doors()
+fnl <- file.path(project_path, "fig", paste(exp,"train-type-setting-errors-difference.pdf",sep="_"))
 ggsave(fnl, plot = last_plot(), unit = "cm", width = width, height = width*.5, limitsize = FALSE)
 
 ### rt
@@ -74,6 +116,19 @@ data %>%
 fnl <- file.path(project_path, "fig", paste(exp,"train-type-rt.pdf",sep="_"))
 ggsave(fnl, plot = last_plot(), unit = "cm", width = width, height = width*.5, limitsize = FALSE)
 
+data %>% 
+  ggplot(aes(x = train_type, y = rt_difference, fill = train_type)) +
+  geom_violin(alpha=.5, width = .6) +
+  geom_boxplot(position = position_dodge(width = .6), width = .05, linewidth = .7, outlier.alpha = 1,outlier.shape = 21,outlier.size = 2.5,outlier.stroke = NA) +
+  scale_fill_manual(values = wes_palette("IsleofDogs1"), labels = c("Stable Context", "Variable Context")) +
+  scale_x_discrete(labels = c("Stable Context", "Variable Context")) +
+  labs(x = "Training Group", y = "Response Time Difference", fill = "Training Group") +
+  theme_minimal() +
+  theme_doors()
+fnl <- file.path(project_path, "fig", paste(exp,"train-type-rt-difference.pdf",sep="_"))
+ggsave(fnl, plot = last_plot(), unit = "cm", width = width, height = width*.5, limitsize = FALSE)
+
+
 ### k4
 if(exp=="exp_lt"){
   data %>% 
@@ -87,6 +142,18 @@ if(exp=="exp_lt"){
     theme_doors()
   fnl <- file.path(project_path, "fig", paste(exp,"train-type-k4.pdf",sep="_"))
   ggsave(fnl, plot = last_plot(), unit = "cm", width = width, height = width*.5, limitsize = FALSE)
+  
+  data %>% 
+    ggplot(aes(x = train_type, y = k4_difference, fill = train_type)) +
+    geom_violin(alpha=.5, width = .6) +
+    geom_boxplot(position = position_dodge(width = .6), width = .05, linewidth = .7, outlier.alpha = 1,outlier.shape = 21,outlier.size = 2.5,outlier.stroke = NA) +
+    scale_fill_manual(values = wes_palette("IsleofDogs1"), labels = c("Stable Context", "Variable Context")) +
+    scale_x_discrete(labels = c("Stable Context", "Variable Context")) +
+    labs(x = "Training Group", y = "Learning Onset Difference", fill = "Training Group") +
+    theme_minimal() +
+    theme_doors()
+  fnl <- file.path(project_path, "fig", paste(exp,"train-type-k4-difference.pdf",sep="_"))
+  ggsave(fnl, plot = last_plot(), unit = "cm", width = width, height = width*.5, limitsize = FALSE)
 }
 
 # -------------------------------------------------------------------------
@@ -94,26 +161,26 @@ if(exp=="exp_lt"){
 data %>% 
   ggplot(aes(x = train_type, y = exploration, fill = train_type)) +
   geom_violin(alpha=.5, width = .6) +
-  geom_boxplot(position = position_dodge(width = .6), width = .05, linewidth = .7, outlier.alpha = 1,outlier.shape = 21,outlier.size = 2.5,outlier.stroke = NA) +
+  geom_boxplot(position = position_dodge(width = .6), width = .02, linewidth = .7, outlier.alpha = 1,outlier.shape = 21,outlier.size = 2.5,outlier.stroke = NA) +
   scale_fill_manual(values = wes_palette("IsleofDogs1"), labels = c("Stable Context", "Variable Context")) +
   labs(x = "", y = "Exploration", fill = "Training Group") +
   guides(x = "none") +
   theme_minimal() +
   theme_doors()
 fnl <- file.path(project_path, "fig", paste(exp,"train-type-exploration.pdf",sep="_"))
-ggsave(fnl, plot = last_plot(), unit = "cm", width = width*.6, height = width*.5, limitsize = FALSE)
+ggsave(fnl, plot = last_plot(), unit = "cm", width = width, height = width*.5, limitsize = FALSE)
 
 data %>% 
   ggplot(aes(x = train_type, y = perseveration, fill = train_type)) +
   geom_violin(alpha=.5, width = .6) +
-  geom_boxplot(position = position_dodge(width = .6), width = .05, linewidth = .7, outlier.alpha = 1,outlier.shape = 21,outlier.size = 2.5,outlier.stroke = NA) +
+  geom_boxplot(position = position_dodge(width = .6), width = .02, linewidth = .7, outlier.alpha = 1,outlier.shape = 21,outlier.size = 2.5,outlier.stroke = NA) +
   scale_fill_manual(values = wes_palette("IsleofDogs1"), labels = c("Stable Context", "Variable Context")) +
   labs(x = "", y = "Perseveration", fill = "Training Group") +
   guides(x = "none") +
   theme_minimal() +
   theme_doors()
 fnl <- file.path(project_path, "fig", paste(exp,"train-type-perseveration.pdf",sep="_"))
-ggsave(fnl, plot = last_plot(), unit = "cm", width = width*.6, height = width*.5, limitsize = FALSE)
+ggsave(fnl, plot = last_plot(), unit = "cm", width = width, height = width*.5, limitsize = FALSE)
 
 # -------------------------------------------------------------------------
 # how are perseveration and exploration distributed?
